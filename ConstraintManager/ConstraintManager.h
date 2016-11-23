155df79af9d141d06157272f1fc2f9324c723ab0
Alexsander Khitev <alexsanderskywork@gmail.com>
2016-11-23 10:49:48 +0400

    Initial Commit

---
+++ ConstraintManager/ConstraintManager.h
@@ -0,0 +1,19 @@
+//
+//  ConstraintManager.h
+//  ConstraintManager
+//
+//  Created by Alexsander Khitev on 11/23/16.
+//  Copyright © 2016 Alexsander Khitev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConstraintManager.
+FOUNDATION_EXPORT double ConstraintManagerVersionNumber;
+
+//! Project version string for ConstraintManager.
+FOUNDATION_EXPORT const unsigned char ConstraintManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConstraintManager/PublicHeader.h>
+
+
